a97573547ba66d119cd85178cf920ac36082e82d
Knight680 <60581999+Knight680@users.noreply.github.com>
2021-07-07 13:49:39 -0400

    Update ops.py

--- ops.py
+++ ops.py
@@ -18,18 +18,41 @@ class Creat_volum_mesh_from_your_creat(bpy.types.Operator):
     Keepratio: bpy.props.FloatProperty(default=1,name="ratio keep for volum mesh(0-1)")
     Maxvolum: bpy.props.FloatProperty(default=100, name="Max_tetrahedraw_volum")
 
-    def func(self):
-        for o in bpy.context.selected_objects:
-            if self.move_axis == "X":
-                o.location.x += self.move_step
-            elif self.move_axis == "Y":
-                o.location.y += self.move_step
-            else:
-                o.location.z += self.move_step
+    def labelmesh(self):
+        bj = bpy.context.view_layer.objects.active
+        # jioned
+        bpy.ops.object.select_all(action='SELECT')
+        if len(bpy.context.selected_objects) >= 2:
+            bpy.ops.object.join()
+
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.mesh.intersect(mode='SELECT', separate_mode='NONE', solver='EXACT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
+        bpy.ops.object.editmode_toggle()
+        # output mesh data to Octave
+        # this works only in object mode,
+        verts = [vert.co for vert in obj.data.vertices]
+        edges = [edge.vertices[:] for edge in obj.data.edges]
+        faces = [face.vertices[:] for face in obj.data.polygons]
+        v = np.array(verts)
+        f = np.array(faces)
+        print("=" * 40)  # printing marker
+
+        oc = op.Oct2Py()
+        oc.addpath(oc.genpath('/Users/zhangyuxuan/Downloads/model/iso2mesh'))
+
+        v = np.array(verts)
+        f = np.array(faces)
+
+        # Save file
+        scipy.io.savemat('result.mat', mdict={'v': v, 'f': f})
+        oc.run('/Users/zhangyuxuan/model/demo_blender.m')
 
     def execute(self, context):
         print("Int ops--->func:")
-        self.func()
+        self.labelmesh()
         print("Utils--->get_scene_objs:")
         get_scene_objs()
         print("Py-Props:")
